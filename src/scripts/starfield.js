e79c756856b5fb3a02264d0250392ebdbdc39148
Christopher Anderson <canderson@maxdigital.com>
2022-03-16 18:50:56 -0500

    Fix star size scale and anchor

--- src/scripts/starfield.js
+++ src/scripts/starfield.js
@@ -41,10 +41,10 @@ export default function createStarfield ({ texture, GAME }) {
       speed: getRandomNum(3, 1),
       tint: i < colorStarThreshold ? genRandomHex(6) : '0xFFFFFF'
     }
-    star.sprite.anchor.x = 0.5
-    star.sprite.anchor.y = 0.7
+    star.sprite.width = star.scale
+    star.sprite.height = star.scale
+    star.sprite.anchor.set(0.5, 0.5)
     star.sprite.tint = star.tint
-    star.sprite.scale.set(star.scale, star.scale)
     star.sprite.x = star.x
     star.sprite.y = star.y
     GAME.stage.addChild(star.sprite)
